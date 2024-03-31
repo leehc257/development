b19aa7cb83dcc400a2bb746af3ee584f57da8cf4
leehc257 <leehc257@leehc257ui-Macmini.local>
2024-03-31 20:49:37 +0900

    docs: file updated

--- lua/plugins/support.lua
+++ lua/plugins/support.lua
@@ -3,6 +3,37 @@ return{
         'chrisbra/csv.vim'
     },
     {
+        'vidocqh/data-viewer.nvim',
+        opts = {},
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "kkharji/sqlite.lua", -- Optional, sqlite support
+        },
+        config = function()
+            require("data-viewer").setup{
+              autoDisplayWhenOpenFile = false,
+              maxLineEachTable = 100,
+              columnColorEnable = false,
+              columnColorRoulette = { -- Highlight groups
+                "DataViewerColumn0",
+                "DataViewerColumn1",
+                "DataViewerColumn2",
+              },
+              view = {
+                float = true, -- False will open in current window
+                width = 0.8, -- Less than 1 means ratio to screen width, valid when float = true
+                height = 0.8, -- Less than 1 means ratio to screen height, valid when float = true
+                zindex = 50, -- Valid when float = true
+              },
+              keymap = {
+                quit = "q",
+                next_table = "<C-l>",
+                prev_table = "<C-h>",
+              },
+            }
+        end
+    },
+    {
         'rcarriga/nvim-notify',
         config = function()
             local plugin = "notify plugin"
