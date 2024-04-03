6646cbd685d577e381e8fc1d88fc16a96ae079b6
leehc257 <leehc257@leehc257ui-Macmini.local>
2024-04-03 19:53:34 +0900

    feat: plugin updated

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -3,6 +3,24 @@ return{
         'majutsushi/tagbar'
     },
     {
+        'kevinhwang91/nvim-hlslens',
+        config = function()
+            local kopts = {noremap = true, silent = true}
+            vim.api.nvim_set_keymap('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+            vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
+            require('hlslens').setup({
+                nearest_only = true,
+                calm_down = true
+            })
+        end
+    },
+    {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' }
