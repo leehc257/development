0096a4d191e986b3e89b34373f0bfcc901edf58e
leehc257 <leehc257@leehc257ui-Macmini.local>
2024-04-13 20:22:15 +0900

    test: support chagned

--- .config/nvim/lua/plugins/support.lua
+++ .config/nvim/lua/plugins/support.lua
@@ -8,7 +8,7 @@ return {
                 },
                 format_on_save = {
                     timeout_ms = 500,
-                    lsp_fallback =true,
+                    lsp_fallback = false,
                 },
             })
             require("conform").formatters.black={
